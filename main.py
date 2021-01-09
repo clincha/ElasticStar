6ee37bc50291376d105a0119763d26c651c62e75
clincha <angus.clinch@gmail.com>
2021-01-09 17:43:24 +0000

    Get accounts complete

--- main.py
+++ main.py
@@ -4,16 +4,25 @@ import requests
 from dotenv import load_dotenv
 
 
-def get_accounts():
+def get_accounts(personal_access_token):
+    """
+    Retuens all the starling Accounts associated with the Personal Access Token
+    :param personal_access_token: Users Personal Access token
+    :return: An array containing all the users accounts. Account object keys:
+        - accountUid
+        - accountType
+        - defaultCategory
+        - currency
+        - createdAt
+        - name
+    """
     parameters = {
-        'Authorization': 'Bearer ' + os.getenv('PERSONAL_ACCESS_TOKEN'),
+        'Authorization': 'Bearer ' + personal_access_token,
     }
-
     response = requests.get("https://api.starlingbank.com/api/v2/accounts", headers=parameters)
-
-    print(response.json())
+    return response.json()['accounts']
 
 
 if __name__ == '__main__':
     load_dotenv()
-    get_accounts()
+    print(get_accounts(os.getenv('PERSONAL_ACCESS_TOKEN'))[0])
