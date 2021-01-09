12dbac86a14fed6b74563c193543718fc5e833fb
clincha <angus.clinch@gmail.com>
2021-01-09 18:06:00 +0000

    Statement periods complete

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 def get_accounts(personal_access_token):
     """
-    Retuens all the starling Accounts associated with the Personal Access Token
+    Get an account holder's bank accounts
     :param personal_access_token: Users Personal Access token
     :return: An array containing all the users accounts. Account object keys:
         - accountUid
@@ -23,6 +23,29 @@ def get_accounts(personal_access_token):
     return response.json()['accounts']
 
 
+def get_statement_periods(personal_access_token, account_uid):
+    """
+    Get list of statement periods which are available for an account
+    :param personal_access_token: Users Personal Access token
+    :param account_uid: The accounts unique identifier
+    :return: An array containing the statement periods. Statement period object keys:
+        - period
+        - partial
+    """
+    parameters = {
+        'Authorization': 'Bearer ' + personal_access_token,
+    }
+    response = requests.get(
+        "https://api.starlingbank.com/api/v2/accounts/" + account_uid + "/statement/available-periods",
+        headers=parameters)
+    return response.json()['periods']
+
+
 if __name__ == '__main__':
     load_dotenv()
-    print(get_accounts(os.getenv('PERSONAL_ACCESS_TOKEN'))[0])
+    pat = os.getenv('PERSONAL_ACCESS_TOKEN')
+
+    accounts = get_accounts(pat)
+    for account in accounts:
+        statement_periods = get_statement_periods(pat, account['accountUid'])
+        print(statement_periods)
