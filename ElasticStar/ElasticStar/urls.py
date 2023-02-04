e59b3bfb0357bb384f9555a824973fed07de4010
clincha <angus.clinch@gmail.com>
2023-02-04 20:27:59 +0000

    /health_check/

--- ElasticStar/ElasticStar/urls.py
+++ ElasticStar/ElasticStar/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.template.defaulttags import url
 from django.urls import path, include
 
 urlpatterns = [
     path('starling/', include('starling.urls')),
+    url(r'^health_check/', include('health_check.urls')),
     path('admin/', admin.site.urls),
 ]
