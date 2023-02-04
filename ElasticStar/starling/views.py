158dd5aa36d35e94c7e7ba71e67a06d8f6d2d663
clincha <angus.clinch@gmail.com>
2023-02-04 16:47:56 +0000

    fix callback URL

--- ElasticStar/starling/views.py
+++ ElasticStar/starling/views.py
@@ -9,6 +9,6 @@ def index(request):
         'client_id': 'BG887EBCs33ZRPzbkfLl',
         'response_type': 'code',
         'state': 'ANGUS12345',
-        'redirect_uri': 'https://clinch-home.com/starling/redirect',
+        'redirect_uri': 'https://elasticstar.clinch-home.com/starling/callback',
     }
     return render(request, 'starling/index.html', context)
