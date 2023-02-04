82f1fb953ed05050641267366da5931fe51ab8f2
clincha <angus.clinch@gmail.com>
2023-02-04 21:45:11 +0000

    no leading slash

--- ElasticStar/starling/urls.py
+++ ElasticStar/starling/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/callback/', views.callback, name='callback')
+    path('callback/', views.callback, name='callback')
 ]
