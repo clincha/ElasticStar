9e7d0a4cae5c1a0d02dbc7f7766752d1b140adc5
clincha <angus.clinch@gmail.com>
2023-02-04 21:42:33 +0000

    callback

--- ElasticStar/ElasticStar/urls.py
+++ ElasticStar/ElasticStar/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.template.defaulttags import url
 from django.urls import path, include
 
 urlpatterns = [
--- ElasticStar/starling/views.py
+++ ElasticStar/starling/views.py
@@ -1,14 +1,53 @@
+import os
+
+import requests
 from django.http import HttpResponse
+from django.http import HttpResponseServerError
 from django.shortcuts import render
 from django.template import loader
+from dotenv import load_dotenv, find_dotenv
+
+client_id = 'BG887EBCs33ZRPzbkfLl'
+redirect_uri = 'https://elasticstar.clinch-home.com/starling/callback'
 
 
 def index(request):
     context = {
         'oath_link': 'https://oauth-sandbox.starlingbank.com/',
-        'client_id': 'BG887EBCs33ZRPzbkfLl',
+        'client_id': client_id,
         'response_type': 'code',
         'state': 'ANGUS12345',
-        'redirect_uri': 'https://elasticstar.clinch-home.com/starling/callback',
+        'redirect_uri': redirect_uri,
     }
     return render(request, 'starling/index.html', context)
+
+
+def callback(request):
+    load_dotenv(find_dotenv())
+    client_secret = os.environ['STARLING_CLIENT_SECRET']
+
+    code = request.GET.get('code')
+    state = request.GET.get('state')
+
+    if state != 'ANGUS12345':
+        return HttpResponseServerError
+
+    headers = {
+        'application': 'x-www-form-urlencoded'
+    }
+
+    parameters = {
+        'code': code,
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'grant_type': 'authorization_code',
+        'redirect_uri': redirect_uri
+    }
+
+    response = requests.get(
+        url='https://api-sandbox.starlingbank.com/oauth/access-token',
+        headers=headers,
+        params=parameters
+    )
+
+    print(response.json())
