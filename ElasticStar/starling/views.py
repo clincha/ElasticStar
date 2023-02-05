4bfbfcb7ce61a07d9b8972a5371f2055d49141e0
clincha <angus.clinch@gmail.com>
2023-02-05 11:16:17 +0000

    lets actually do it

--- ElasticStar/starling/views.py
+++ ElasticStar/starling/views.py
@@ -1,13 +1,20 @@
 import os
 
 import requests
+import tqdm
 from django.http import HttpResponseServerError
 from django.http import JsonResponse
 from django.shortcuts import render
 from dotenv import load_dotenv, find_dotenv
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import streaming_bulk
+
+from ElasticStar.starling.starling import Starling
 
 client_id = 'BG887EBCs33ZRPzbkfLl'
 redirect_uri = 'https://elasticstar.clinch-home.com/starling/callback'
+
+elastic_index = 'elasticstar'
 
 
 def index(request):
@@ -45,6 +52,24 @@ def callback(request):
     )
 
     if response.status_code == 200:
+        starling = Starling(response.json()['access_token'])
+        main_account = starling.get_accounts()[0]['accountUid']
+        transactions = starling.get_transaction_feed(main_account)
+        print("Success!")
+
+        print("Adding transactions to Elastic...")
+        elastic = Elasticsearch(
+            cloud_id=os.getenv("ELASTIC_CLOUD_ID"),
+            basic_auth=("elastic", os.getenv("ELASTIC_CLOUD_PASSWORD"))
+        )
+        progress = tqdm.tqdm(unit="documents", total=sum(1 for _ in transactions['feedItems']))
+        for ok, action in streaming_bulk(
+                client=elastic,
+                index=elastic_index,
+                actions=starling.generate_elastic_bulk_actions(transactions)
+        ):
+            progress.update(1)
+
         return JsonResponse(response.json())
     else:
         return HttpResponseServerError(str(response.status_code) + ' - ' + response.reason)
