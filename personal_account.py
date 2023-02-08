8e30bdcb7932a5c217bb146648da19f442aa9fec
clincha <angus.clinch@gmail.com>
2023-02-08 20:39:18 +0000

    type

--- personal_account.py
+++ personal_account.py
@@ -7,8 +7,6 @@ from elasticsearch.helpers import streaming_bulk
 
 from ElasticStar.starling.Starling import Starling
 
-elastic_index = "clincha_starling_personal"
-
 accounts = [
     {"type": "personal"},
     {"type": "business"}
@@ -20,7 +18,7 @@ if __name__ == '__main__':
     for account in accounts:
         print("Getting transaction history...")
         starling = Starling(os.getenv(
-            '{account_type}_ACCESS_TOKEN'.format(account_type=str(account).capitalize())),
+            '{account_type}_ACCESS_TOKEN'.format(account_type=account['type'].capitalize())),
             sandbox=False)
         main_account = starling.get_accounts()[0]['accountUid']
         transactions = starling.get_transaction_feed(main_account)
@@ -31,11 +29,12 @@ if __name__ == '__main__':
             cloud_id=os.getenv("ELASTIC_CLOUD_ID"),
             basic_auth=("elastic", os.getenv("ELASTIC_CLOUD_PASSWORD"))
         )
+        elastic_index = "CLINCHA_STARLING_{account_type}".format(account_type=account['type'].capitalize())
         elastic.indices.create(index=elastic_index, ignore=400)  # ignore 400 (IndexAlreadyExistsException)
         progress = tqdm.tqdm(unit="documents", total=sum(1 for _ in transactions['feedItems']))
         for ok, action in streaming_bulk(
                 client=elastic,
-                index="CLINCHA_STARLING_{account_type}".format(account_type=str(account).capitalize()),
+                index=elastic_index,
                 actions=starling.generate_elastic_bulk_actions(transactions)
         ):
             progress.update(1)
