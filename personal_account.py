3b1c7e813a1aa2c7acdd5f912a4a3c04092ac3f1
clincha <angus.clinch@gmail.com>
2023-02-09 21:27:06 +0000

    business

--- personal_account.py
+++ personal_account.py
@@ -9,17 +9,12 @@ from ElasticStar.starling.Starling import Starling
 
 if __name__ == '__main__':
     load_dotenv()
-
-    accounts = [
-        {"type": "personal"},
-        {"type": "business"}
-    ]
+    accounts = ['PERSONAL', 'BUSINESS', 'JOINT']
 
     for account in accounts:
-        print("Getting transaction history...")
-        print('{account_type}_ACCESS_TOKEN'.format(account_type=account['type'].capitalize()))
+        print("Getting transaction history for account type: {account_type}".format(account_type=account))
         starling = Starling(os.getenv(
-            '{account_type}_ACCESS_TOKEN'.format(account_type=account['type'].capitalize())),
+            '{account_type}_ACCESS_TOKEN'.format(account_type=account)),
             sandbox=False)
         main_account = starling.get_accounts()[0]['accountUid']
         transactions = starling.get_transaction_feed(main_account)
@@ -30,7 +25,7 @@ if __name__ == '__main__':
             cloud_id=os.getenv("ELASTIC_CLOUD_ID"),
             basic_auth=("elastic", os.getenv("ELASTIC_CLOUD_PASSWORD"))
         )
-        elastic_index = "CLINCHA_STARLING_{account_type}".format(account_type=account['type'].capitalize())
+        elastic_index = "CLINCHA_STARLING_{account_type}".format(account_type=account)
         elastic.indices.create(index=elastic_index, ignore=400)  # ignore 400 (IndexAlreadyExistsException)
         progress = tqdm.tqdm(unit="documents", total=sum(1 for _ in transactions['feedItems']))
         for ok, action in streaming_bulk(
