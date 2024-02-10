fdc451c00acc9729c1a713fa4a79b837adb187c0
clincha <angus.clinch@gmail.com>
2024-02-10 17:13:46 +0000

    Spaces

--- starling_to_sheets.py
+++ starling_to_sheets.py
@@ -4,70 +4,114 @@ import gspread
 import variables
 from starling import Starling
 
+
+def update_transaction_sheet(account, transactions, workbook):
+    print(f"Accessing sheet for {account.lower()} account...")
+    try:
+        worksheet = workbook.worksheet(f"starling-{account.lower()}")
+    except gspread.exceptions.WorksheetNotFound:
+        worksheet = workbook.add_worksheet(f"starling-{account.lower()}", 0, 0)
+
+    print(f"Formatting transaction data...")
+    data = [[
+        "Currency",
+        "Amount",
+        "Source Currency",
+        "Source Amount",
+        "Direction",
+        "Transaction Time",
+        "Source",
+        "Status",
+        "Counter Party Type",
+        "Counter Party Name",
+        "Reference",
+        "Country",
+        "Spending Category",
+        "Has Attachment",
+        "Has Receipt"
+    ]]
+    for transaction in transactions:
+        try:
+            data.append([
+                transaction.get('amount', "").get('currency', ""),
+                transaction.get('amount', "").get('minorUnits', ""),
+                transaction.get('sourceAmount', "").get('currency', ""),
+                transaction.get('sourceAmount', "").get('minorUnits', ""),
+                transaction.get('direction', ""),
+                transaction.get('transactionTime', ""),
+                transaction.get('source', ""),
+                transaction.get('status', ""),
+                transaction.get('counterPartyType', ""),
+                transaction.get('counterPartyName', ""),
+                transaction.get('reference', ""),
+                transaction.get('country', ""),
+                transaction.get('spendingCategory', ""),
+                transaction.get('hasAttachment', ""),
+                transaction.get('hasReceipt', ""),
+            ])
+        except KeyError as missing_key:
+            # Instead of failing here we can just set the values to "" if they don't have data in the API
+            print(f"Missing key for {missing_key}. Fatal transaction {transaction}")
+
+    print(f"Adding transactions to sheet...")
+    worksheet.update(range_name='A1',
+                     values=data)
+
+
+def update_saving_spaces_sheet(account, spaces, workbook):
+    print(f"Accessing saving spaces sheet for {account.lower()} account...")
+    try:
+        worksheet = workbook.worksheet(f"starling-spaces-{account.lower()}")
+    except gspread.exceptions.WorksheetNotFound:
+        worksheet = workbook.add_worksheet(f"starling-spaces-{account.lower()}", 0, 0)
+
+    print(f"Formatting saving spaces data...")
+    data = [[
+        "Space",
+        "Target Currency",
+        "Target",
+        "Total Saved Currency",
+        "Total Saved",
+        "Saved Percentage",
+        "State"
+    ]]
+    for space in spaces['savingsGoalList']:
+        data.append(
+            [
+                space.get('name', ""),
+                space.get('target', "").get('currency', ""),
+                space.get('target', "").get('minorUnits', ""),
+                space.get('totalSaved', "").get('currency', ""),
+                space.get('totalSaved', "").get('minorUnits', ""),
+                space.get('savedPercentage', ""),
+                space.get('state', "")
+            ]
+        )
+    print(f"Adding saving space data to sheet...")
+    worksheet.update(range_name='A1',
+                     values=data)
+
+
 if __name__ == '__main__':
-    for account in variables.accounts:
-        access_token = f'{account}_ACCESS_TOKEN'
+    print(f"Connecting to Google Sheets...")
+    finance_workbook = gspread.service_account(filename="service_account.json").open("Finance")
+
+    for starling_account in variables.accounts:
+        print(f"Checking for {starling_account.lower()} access token...")
+        access_token = f'{starling_account}_ACCESS_TOKEN'
         if access_token not in os.environ:
-            # Skip to the next account type
+            print("Not found, skipping...")
             continue
 
-        print(f"Accessing sheet for {account.lower()} account...")
-        gc = gspread.service_account(filename="service_account.json")
-        try:
-            worksheet = gc.open("Finance").worksheet(f"starling-{account.lower()}")
-        except gspread.exceptions.WorksheetNotFound:
-            worksheet = gc.open("Finance").add_worksheet(f"starling-{account.lower()}", 0, 0)
-
-        print(f"Getting transaction history for {account.lower()} account...")
+        print(f"Getting data from {starling_account.lower()} account...")
         starling = Starling(
             os.getenv(access_token),
             sandbox=False)
         main_account = starling.get_accounts()[0]['accountUid']
-        transactions = starling.get_transaction_feed(main_account)
+        account_transactions = starling.get_transaction_feed(main_account)
+        account_spaces = starling.get_saving_spaces(main_account)
 
-        print(f"Formatting transaction data...")
-        data = [[
-            "Currency",
-            "Amount",
-            "Source Currency",
-            "Source Amount",
-            "Direction",
-            "Transaction Time",
-            "Source",
-            "Status",
-            "Counter Party Type",
-            "Counter Party Name",
-            "Reference",
-            "Country",
-            "Spending Category",
-            "Has Attachment",
-            "Has Receipt"
-        ]]
-        for index, transaction in enumerate(transactions):
-            try:
-                data.append([
-                    transaction['amount']['currency'],
-                    transaction['amount']['minorUnits'],
-                    transaction['sourceAmount']['currency'],
-                    transaction['sourceAmount']['minorUnits'],
-                    transaction.get('direction', ""),
-                    transaction.get('transactionTime', ""),
-                    transaction.get('source', ""),
-                    transaction.get('status', ""),
-                    transaction.get('counterPartyType', ""),
-                    transaction.get('counterPartyName', ""),
-                    transaction.get('reference', ""),
-                    transaction.get('country', ""),
-                    transaction.get('spendingCategory', ""),
-                    transaction.get('hasAttachment', ""),
-                    transaction.get('hasReceipt', ""),
-                ])
-            except KeyError as missing_key:
-                # Instead of failing here we can just set the values to "" if they don't have data in the API
-                print(f"Missing key for {missing_key}. Fatal transaction {transaction}")
+        # update_transaction_sheet(starling_account, account_transactions, finance_workbook)
+        update_saving_spaces_sheet(starling_account, account_spaces, finance_workbook)
 
-        print(f"Adding transactions to sheet...")
-        worksheet.update(range_name='A1',
-                         values=data)
-
-        print("Done!")
+        print(f"Successfully finished updating {starling_account.lower()} account!")
