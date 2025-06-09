cf12b88662ce32b9752e5794549baca7b831f130
clincha <angus.clinch@gmail.com>
2025-06-09 20:09:07 +0100

    workflows

--- terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "elasticsearch_endpoint" {
-  value = ec_deployment.production.elasticsearch[0].https_endpoint
-}
-
-output "kibana_endpoint" {
-  value = ec_deployment.production.kibana[0].https_endpoint
-}
