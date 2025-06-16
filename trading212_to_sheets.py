be1567af45732b5457bb7d3b1791194beb31c653
clincha <angus.clinch@gmail.com>
2025-06-16 08:03:37 +0100

    position data

--- trading212_to_sheets.py
+++ trading212_to_sheets.py
@@ -21,3 +21,21 @@ for account in accounts:
     for key, value in response.items():
         data.append([key, value])
     worksheet.update(range_name="A1", values=data)
+
+
+for account in accounts:
+    api_key = os.getenv(f"TRADING212_{account}_API_KEY")
+    t212 = client.Client(api_key)
+    positions = t212.get_open_positions()
+
+    try:
+        worksheet = finance_workbook.worksheet(f"trading212-{account.lower()}-positions")
+    except gspread.exceptions.WorksheetNotFound:
+        worksheet = finance_workbook.add_worksheet(f"trading212-{account.lower()}-positions", 0, 0)
+
+    print(positions)
+
+    data = []
+    for position in positions:
+        row = [position['ticker'], position['quantity'], position['currentPrice'], position['ppl'], position['fxPpl']]
+        data.append(row)
