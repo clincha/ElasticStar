1efaee2620315c652ada8b9aa97f9f74006f9a0d
clincha <angus.clinch@gmail.com>
2025-06-17 20:17:02 +0100

    tidy up and tighten

--- trading212_to_sheets.py
+++ trading212_to_sheets.py
@@ -10,23 +10,15 @@ finance_workbook = gspread.service_account(filename="service_account.json").open
 for account in accounts:
     api_key = os.getenv(f"TRADING212_{account}_API_KEY")
     t212 = client.Client(api_key)
-    response = t212.get_account_cash()
 
+    # Get account cash
     try:
         worksheet = finance_workbook.worksheet(f"trading212-{account.lower()}")
     except gspread.exceptions.WorksheetNotFound:
         worksheet = finance_workbook.add_worksheet(f"trading212-{account.lower()}", 0, 0)
+    worksheet.update(range_name="A1", values=[[key, value] for key, value in t212.get_account_cash().items()])
 
-    data = []
-    for key, value in response.items():
-        data.append([key, value])
-    worksheet.update(range_name="A1", values=data)
-
-for account in accounts:
-    api_key = os.getenv(f"TRADING212_{account}_API_KEY")
-    t212 = client.Client(api_key)
-    positions = t212.get_open_positions()
-
+    # Get account positions
     try:
         worksheet = finance_workbook.worksheet(f"trading212-{account.lower()}-positions")
     except gspread.exceptions.WorksheetNotFound:
@@ -44,15 +36,11 @@ for account in accounts:
     ]]
 
     all_instruments = t212.get_instruments()
-    row_index = 1
-    for position in positions:
-        # Get the instrument details for the ticker
-        instrument = next((inst for inst in all_instruments if inst['ticker'] == position['ticker']), None)
-        row_index = row_index + 1
+    for index, position in enumerate(t212.get_open_positions()):
         row = [
             position['ticker'],
             position['quantity'],
-            instrument['currencyCode'],
+            (inst['currencyCode'] for inst in all_instruments if inst['ticker'] == position['ticker']),
             position['averagePrice'],
             position['currentPrice'],
             position['ppl'],
@@ -60,14 +48,14 @@ for account in accounts:
             f'='
             f'('
             f'  SWITCH('
-            f'          C{row_index},'
+            f'          C{index},'
             f'          "GBP", 1,'
             f'          "GBX", 0.01,'
-            f'          GOOGLEFINANCE(CONCATENATE("Currency:"&C{row_index}&"GBP"))'
-            f'      ) ' # Get the conversion rate to GBP
-            f'  * E{row_index}' # Current Price in local currency
+            f'          GOOGLEFINANCE(CONCATENATE("Currency:"&C{index}&"GBP"))'
+            f'      ) '  # Get the conversion rate to GBP
+            f'  * E{index}'  # Current Price in local currency
             f')'
-            f' * B{row_index}' # Quantity
+            f' * B{index}'  # Quantity
         ]
         data.append(row)
     worksheet.update(range_name="A1", values=data, raw=False)
